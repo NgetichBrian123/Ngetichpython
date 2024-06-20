4da50a216fb18ba2bb7eb187cc7cd9c5c8c4dc62
NgetichBrian123 <Kngetich00@gmail.com>
2024-06-20 15:11:15 +0300

    first commit

---
+++ Variables.py
@@ -0,0 +1,14 @@
+
+#Variable is a container used to store a value
+name ="Brian"
+programmingSchool ="eMobilis"
+num = 67
+num2 = 3
+age = 67
+
+print("The sum of",num,"and",num2,'is',num+num2)
+print("I am",age,"years old")
+print("My name is",name,"I study at",programmingSchool)
+print(name)
+print(num)
+print("My name is",name)
---
+++ datatype.py
@@ -0,0 +1,30 @@
+
+
+number = 78 # Int
+X = 67.32 # Float
+greeting = "hello there" # String
+isPythonInteresting = True # Bool
+devices =["laptop","computer","tablet"] # List
+browsers =("opera","firefox","chrome","safaricom") #Tuple
+countries ={"Kenya","Sudan","Rwanda"} # Set
+student ={
+    "firstname":"Tevin",
+    "age": 18,
+    "course":"MIT"
+} # Dictionary
+print(isPythonInteresting)
+print(X)
+print(number)
+print(devices)
+print(browsers)
+print(student["firstname"])
+
+
+
+# Determining the datatype
+print(type(devices))
+print(type(student))
+
+# Typecasting -is the process of converting one datatype
+print(int(X))
+print(float(number))
